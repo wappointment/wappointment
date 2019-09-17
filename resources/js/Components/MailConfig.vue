0de219bbfc74a8ee0fe279a5dcd6ea21dec1e711
Benjamin Caubere <b.caubere@gmail.com>
2019-09-17 14:48:45 +0200

    Added informative message

--- resources/js/Components/MailConfig.vue
+++ resources/js/Components/MailConfig.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <NoticeButton :config="nb" @click="showConfiguration" v-if="!mail_config_ok"></NoticeButton>
-        <div v-else class="d-flex align-items-center">
-            <span class="dashicons dashicons-yes text-success mail_configured"></span>
-            <span>Email sending method configured</span>
-            <button class="ml-2 btn btn-sm btn-secondary" @click="showConfiguration">Reconfigure it</button>
+        <div v-else >
+            <div class="d-flex align-items-center">
+                <span class="dashicons dashicons-yes text-success mail_configured"></span>
+                <span>Email sending method configured</span>
+                <button class="ml-2 btn btn-sm btn-secondary" @click="showConfiguration">Reconfigure it</button>
+            </div>
+            <div class="small text-muted ml-2"><span class="text-dark">Please make sure you receive emails.</span> If you have deliverability issues, reconfigure it with Mailgun, it is free and it just works.</div>
         </div>
          <WapModal id="configureMail" ref="configureMail" v-if="showModal" :show="showModal" @hide="hideModal">
             <h4 slot="title" class="modal-title">{{titleModal}}</h4>
