914a53afadcab9763a803cf30a11ac27b65003f4
Benjamin Caubere <b.caubere@gmail.com>
2019-10-07 12:47:30 +0200

    fixed staff retrievals on custom prefix or ms

--- app/Models/WPUserMeta.php
+++ app/Models/WPUserMeta.php
@@ -18,7 +18,12 @@ class WPUserMeta extends Model
         foreach ($roles as $role) {
             $wp_capabilities[] = serialize([$role => true]);
         }
-        $result_user_ids = self::select('user_id')->where('meta_key', 'wp_capabilities')->whereIn('meta_value', $wp_capabilities)->get();
+
+        global $wpdb;
+        $site_prefix = $wpdb->get_blog_prefix();
+
+        $result_user_ids = self::select('user_id')->where('meta_key', $site_prefix . 'capabilities')->whereIn('meta_value', $wp_capabilities)->get();
+
         $user_ids = [];
         foreach ($result_user_ids->toArray() as $array) {
             $user_ids[] = $array['user_id'];
