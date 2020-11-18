b1b34a179412c8bd2c2bb4d12966b05f85d5f953
Benjamin Caubere <b.caubere@gmail.com>
2020-11-18 12:27:34 +0100

    attempt to fix an installation error SQLSTATE[23000]: Integrity constraint violation: 1022 Cant write; duplicate key

--- database/migrations/2018_07_01_000000_create_appointments_table.php
+++ database/migrations/2018_07_01_000000_create_appointments_table.php
@@ -21,7 +21,8 @@ class CreateAppointmentsTable extends Wappointment\Installation\Migrate
             $table->string('edit_key', 32)->nullable();
             $table->timestamps();
             $table->mediumText('options')->nullable();
-            $table->integer('client_id')->foreign()->references('id')->on(Database::$prefix_self . '_clients');
+            $table->integer('client_id')->nullable()->default(null);
+            //$table->unsignedInteger('client_id')->foreign()->references('id')->on(Database::$prefix_self . '_clients');
             $table->unsignedTinyInteger('staff_id')->default(0);
             $table->unsignedTinyInteger('service_id')->default(0);
             $table->unique(['staff_id', 'status', 'start_at', 'end_at'], 'unique_staff_id_status_start_end');
