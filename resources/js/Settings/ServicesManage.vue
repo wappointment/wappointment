fab3f3530a6a9ec59e1a9a68a3fe6707fd5a79dd
Benjamin Caubere <b.caubere@gmail.com>
2021-02-09 17:32:52 +0100

    legacy work

--- resources/js/Settings/ServicesManage.vue
+++ resources/js/Settings/ServicesManage.vue
@@ -2,14 +2,14 @@
     <div >
         <div v-if="serviceListing">
             <button @click="showService" class="btn btn-outline-primary btn my-2">Add new</button>
-            <div class="table-responsive table-hover">
+            <div class="table-responsive table-hover" v-if="elements.services !== undefined">
                 <table class="table">
                     <thead>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Service</th>
                             <th scope="col">Durations</th>
-                            <th scope="col">Delivery Modalities <a href="javascript:;" @click="$emit('changeView', 'delivery')">Manage</a></th>
+                            <th scope="col">Delivery Modalities <a href="javascript:;" v-if="!requiresDBUpgrade" @click="$emit('changeView', 'delivery')">Manage</a></th>
                         </tr>
                     </thead>
                     <draggable @change="orderChanged" v-model="elements.services" draggable=".row-click" handle=".dashicons-move" tag="tbody" v-if="elements.services.length > 0">
@@ -22,22 +22,22 @@
                                 <div class="d-flex align-items-center">
                                     <WapImage :element="service" :config="{mauto:false}" :desc="service.name" size="lg" /> <div class="ml-2">{{ service.name }}</div>
                                     <div class="actions ml-4 text-muted">
-                                        <span data-tt="Sort"><span class="dashicons dashicons-move"></span></span>
+                                        <span data-tt="Sort" v-if="elements.services.length > 1" ><span class="dashicons dashicons-move"></span></span>
                                         <span data-tt="Edit"><span class="dashicons dashicons-edit" @click.prevent.stop="editElement(service)"></span></span>
-                                        <span data-tt="Delete"><span class="dashicons dashicons-trash" @click.prevent.stop="deleteService(service.id)"></span></span>
-                                        <span>(id: {{ service.id }})</span>
+                                        <span data-tt="Delete" v-if="elements.services.length > 1" ><span class="dashicons dashicons-trash" @click.prevent.stop="deleteService(service.id)"></span></span>
+                                        <span v-if="elements.services.length > 1" >(id: {{ service.id }})</span>
                                     </div>
                                 </div>
                                 
                             </td>
                             <td>
                                 <div class="d-flex">
-                                    <DurationCell v-for="(durationObj,dkey) in service.options.durations" :key="dkey" :show="true" :duration="durationObj.duration"/>
+                                    <DurationCell v-for="(durationObj,dkey) in getDurations(service)" :key="dkey" :show="true" :duration="durationObj.duration"/>
                                 </div>
                             </td>
                             <td>
                                 <div class="d-flex">
-                                    <div v-for="locationObj in service.locations" class="location d-flex align-items-center" :data-tt="locationObj.name">
+                                    <div v-for="locationObj in getLocations(service)" class="location d-flex align-items-center" :data-tt="locationObj.name">
                                         <WapImage :element="locationObj" :desc="locationObj.name" size="md" />
                                     </div>
                                 </div>
@@ -56,7 +56,8 @@
         </div>
         <div v-if="serviceAdd">
             <button class="btn btn-link btn-xs mb-2" @click="showListing"> < Back</button>
-            <ServicesAddEdit :element="elementPassed" @saved="hasBeenSavedDeleted"/>
+            <ServicesEditLegacy v-if="currentView=='editLegacy'" :element="elementPassed" @saved="hasBeenSavedDeleted"/>
+            <ServicesAddEdit v-else :element="elementPassed" @saved="hasBeenSavedDeleted"/>
         </div>
     </div>
 </template>
@@ -65,11 +66,13 @@
 
 import WappoServiceService from '../Services/V1/Services'
 import ServicesAddEdit from './ServicesAddEdit'
+import ServicesEditLegacy from '../Views/Subpages/Service'
 export default {
     extends: window.wappoGet('AbstractListing'),
     components:{
         DurationCell: window.wappoGet('DurationCell'),
-        ServicesAddEdit
+        ServicesAddEdit,
+        ServicesEditLegacy
     },
     data: () => ({
         currentView: 'listing',
@@ -85,11 +88,42 @@ export default {
             return this.currentView == 'listing'
         },
         serviceAdd(){
-            return ['add','edit'].indexOf(this.currentView) !== -1
+            return ['add','edit','editLegacy'].indexOf(this.currentView) !== -1
+        },
+        requiresDBUpgrade(){
+            return this.elements.db_required 
         }
     },
     methods: {
-
+        isLegacy(service){
+            return service.options.durations === undefined
+        },
+        getDurations(service){
+            if(!this.isLegacy(service)){
+                return service.options.durations
+            }else{
+                return [
+                    {
+                        duration: service.duration
+                    }
+                ]
+            }
+        },
+        getLocations(service){
+            if(!this.isLegacy(service)){
+                return service.options.types
+            }else{
+                let newTypes = []
+                for (let i = 0; i < service.type.length; i++) {
+                    newTypes.push({
+                        name: service.type[i],
+                        type: service.type[i],
+                        options:{}
+                    })
+                }
+                return newTypes
+            }
+        },
         loadElements() { // overriding
             if(this.currentView == 'listing') {
                 this.request(this.requestElements,{},undefined,false,this.loadedElements,this.failedLoadingElements)
@@ -133,12 +167,12 @@ export default {
                     { target: 'goToServiceAdd', label: 'Edit' , disabled:true},
                 ], 'edit', {element:element})
             }else{
-                this.currentView = 'edit'
+                this.currentView = this.requiresDBUpgrade ? 'editLegacy':'edit'
                 this.elementPassed = element
             }
         },
         showService(){
-            if(this.elements.db_required){
+            if(this.requiresDBUpgrade){
                 return this.$WapModal().notifyError('Run database updates first')
             }
             if(this.elements.services.length > 2){
