c6289b36711011eac4b050655bbde6b3f95ea425
Benjamin Caubere <b.caubere@gmail.com>
2021-03-26 17:31:40 +0100

    reset fix

--- app/Services/Reset.php
+++ app/Services/Reset.php
@@ -10,13 +10,36 @@ use Wappointment\Services\Wappointment\DotCom;
 
 class Reset
 {
+    private $options = [
+        'flags',
+        'wizard_step',
+        'subscribed_status',
+        'widget_settings',
+        'site_details',
+        'site_key',
+        'installation_completed',
+        'installation_step',
+        'db_version_created',
+        'db_version',
+    ];
+
+    private $user_options = [
+        'availability',
+        'calendar_logs',
+        'cal_urls',
+        'viewed_updates',
+        'hello_page',
+        'preferences',
+        'since_last_refresh',
+    ];
+
     public function __construct()
     {
 
         do_action('wappointment_reset');
         sleep(2); //giving time for revert on addons
 
-        //$this->dotComInforms();
+        $this->dotComInforms();
 
         $this->removeStaffSettings();
         $this->dropTables();
@@ -27,9 +50,8 @@ class Reset
 
     private function dotComInforms()
     {
-        $acs_id = Settings::get('activeStaffId');
         $dotcomapi = new DotCom;
-        $dotcomapi->setStaff($acs_id);
+        $dotcomapi->setStaff();
         $dotcomapi->notifyReset();
     }
 
@@ -37,13 +59,9 @@ class Reset
     {
         foreach (Staff::getIds() as $staff_id) {
             Settings::deleteStaff($staff_id);
-            WPHelpers::deleteStaffOption('availability', $staff_id);
-            WPHelpers::deleteStaffOption('calendar_logs', $staff_id);
-            WPHelpers::deleteStaffOption('cal_urls', $staff_id);
-            WPHelpers::deleteStaffOption('viewed_updates', $staff_id);
-            WPHelpers::deleteStaffOption('hello_page', $staff_id);
-            WPHelpers::deleteStaffOption('preferences', $staff_id);
-            WPHelpers::deleteStaffOption('since_last_refresh', $staff_id);
+            foreach ($this->user_options as $option_key) {
+                WPHelpers::deleteStaffOption($option_key, $staff_id);
+            }
         }
     }
 
@@ -51,11 +69,7 @@ class Reset
     private function dropTables()
     {
         $migrate = new \Wappointment\Installation\Migrate();
-        try {
-            $migrate->rollback();
-        } catch (\Throwable $th) {
-            throw new \WappointmentException("Error while DROPPING DB tables " . $th, 1);
-        }
+        $migrate->rollback();
 
         Capsule::schema()->dropIfExists(Database::$prefix_self . '_migrations');
         if (Capsule::schema()->hasTable(Database::$prefix_self . '_migrations')) {
@@ -65,20 +79,9 @@ class Reset
 
     private function removeCoreSettings()
     {
-        WPHelpers::deleteOption('flags');
-        WPHelpers::deleteOption('wizard_step');
-        WPHelpers::deleteOption('subscribed_status');
-
-        WPHelpers::deleteOption('widget_settings');
-
-        WPHelpers::deleteOption('site_details');
-        WPHelpers::deleteOption('site_key');
-
-        WPHelpers::deleteOption('installation_completed');
-        WPHelpers::deleteOption('installation_step');
-
-        WPHelpers::deleteOption('db_version');
-
+        foreach ($this->options as $option_key) {
+            WPHelpers::deleteOption($option_key);
+        }
 
         Settings::delete();
     }
