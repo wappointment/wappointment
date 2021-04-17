248475cb2ed272a4e99d499e91f0e2909c4460e7
Benjamin Caubere <b.caubere@gmail.com>
2021-04-17 06:58:22 +0200

    added week param in shortcode

--- resources/js/Components/WidgetInsert.vue
+++ resources/js/Components/WidgetInsert.vue
@@ -3,13 +3,13 @@
         <div class="text-muted mt-4">Insert the booking form in a new page, within an existing page or within a widget area</div>
         <div class="d-flex my-4" >
             <button class="btn btn-secondary btn-cell btn-xs ml-0 mr-2" :class="{selected: showPage}" @click="showArea('page')">
-                <span class="dashicons dashicons-welcome-add-page"></span> Create a new page
+                <span class="dashicons dashicons-welcome-add-page mr-1"></span> Create a new page
             </button>
             <button class="btn btn-secondary btn-cell btn-xs ml-0 mr-2" :class="{selected: showPost}" @click="showArea('post')">
-                <span class="dashicons dashicons-shortcode"></span> Using a Shortcode
+                <span class="dashicons dashicons-shortcode mr-1"></span> Using a Shortcode
             </button>
             <button class="btn btn-secondary btn-cell btn-xs ml-0 mr-2" :class="{selected: showWidget}" @click="showArea('widget')">
-                <span class="dashicons dashicons-welcome-widgets-menus"></span> Using our Widget
+                <span class="dashicons dashicons-welcome-widgets-menus mr-1"></span> Using our Widget
             </button>
         </div>
         <div v-if="showPage">
