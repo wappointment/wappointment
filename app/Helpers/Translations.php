9dfddfec0f96f8c8d7a04f8839c156d61ed05fe9
Benjamin Caubere <b.caubere@gmail.com>
2021-04-29 21:57:15 +0200

    adde trans

---
+++ app/Helpers/Translations.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Wappointment\Helpers;
+
+class Translations
+{
+    public function parse()
+    {
+        __('With appointment', 'wappointment');
+        __('Starts at', 'wappointment');
+    }
+}
