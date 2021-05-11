fa524f671cd00d350124ab8d046c071e674d4f43
Benjamin Caubere <b.caubere@gmail.com>
2021-05-11 18:30:51 +0200

    fixed import from mixed versions

--- database/migrations/2021_01_02_000005_import_service.php
+++ database/migrations/2021_01_02_000005_import_service.php
@@ -5,6 +5,7 @@ use Wappointment\Models\Location;
 use Wappointment\Models\Service as ServiceModel;
 use Wappointment\System\Status;
 use Wappointment\Services\Service;
+use Wappointment\Services\Services;
 
 class ImportService extends Wappointment\Installation\MigrateHasServices
 {
@@ -24,6 +25,7 @@ class ImportService extends Wappointment\Installation\MigrateHasServices
         $service_free = \Wappointment\Services\Service::getObject();
 
         \Wappointment\Services\Service::updateLocations($service_free->type, $service_free->options, $service_free->address);
+
 
         if ($service_free->service['name'] !== '') { //doesnt run for new installation just upgrades
             $serviceObj = ServiceModel::create([
