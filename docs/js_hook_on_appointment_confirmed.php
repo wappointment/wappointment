1543053c3adf2ab20d4cd15b05ec7b3013aaaaec
Benjamin Caubere <b.caubere@gmail.com>
2021-06-03 13:03:04 +0200

    updated doc

--- docs/js_hook_on_appointment_confirmed.php
+++ docs/js_hook_on_appointment_confirmed.php
@@ -14,7 +14,13 @@
  *
  * @return void
  */
-function my_wappo_footer_function()
+
+/**
+ * when an appointment is confirmed
+ *
+ * @return void
+ */
+function my_wappo_analytics_appointment_confirmed()
 {
 
   echo '<script>';
@@ -24,4 +30,22 @@ function my_wappo_footer_function()
             }, false);";
   echo '</script>';
 }
-add_action('wp_print_footer_scripts', 'my_wappo_footer_function');
+add_action('wp_print_footer_scripts', 'my_wappo_analytics_appointment_confirmed');
+
+
+/**
+ * when an appointment is rescheduled
+ *
+ * @return void
+ */
+function my_wappo_analytics_appointment_rescheuled()
+{
+
+  echo '<script>';
+  echo "document.addEventListener('wappo_rescheduled', function (e) {
+              // window.location.replace('https://mysite.com/myurl');
+              console.log('appointment Data', e.wdata )
+            }, false);";
+  echo '</script>';
+}
+add_action('wp_print_footer_scripts', 'my_wappo_analytics_appointment_rescheuled');
