5c2db951d09f485a5ff948e19b76e56a851c30f1
Benjamin Caubere <b.caubere@gmail.com>
2021-08-13 12:19:18 +0200

    fixed minor issue availability

--- app/Services/Availability.php
+++ app/Services/Availability.php
@@ -207,12 +207,11 @@ class Availability
         $dayNumber = 1;
         $now = Carbon::today($this->timezone);
         $min_time = Carbon::now($this->timezone)->addHours(Settings::get('hours_before_booking_allowed'));
-        $max_time = Carbon::now($this->timezone)->addDays($this->days);
+        $max_time = Carbon::now($this->timezone)->addDays($this->days)->endOfDay();
         $availability = [];
 
         while ($now->timestamp < $max_time->timestamp) {
             $dayName = $this->daysOfTheWeek[$now->dayOfWeek];
-
             $dailyAvailability = $this->regav[$dayName];
 
             foreach ($dailyAvailability as $dayTimeblock) {
