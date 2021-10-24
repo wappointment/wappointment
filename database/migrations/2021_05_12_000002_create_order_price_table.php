881fa067abd43dcdb51f5b7c9b30e799c89d51d1
Benjamin Caubere <b.caubere@gmail.com>
2021-10-24 19:45:26 +0200

    reduce foreign key length

--- database/migrations/2021_05_12_000002_create_order_price_table.php
+++ database/migrations/2021_05_12_000002_create_order_price_table.php
@@ -16,35 +16,37 @@ class CreateOrderPriceTable extends Wappointment\Installation\Migrate
         $foreignNameOrder = $this->getFKOrders();
         $foreignNamePrice = $this->getFKPrices();
         $foreignNameAppointments = $this->getFKAppointments();
-        Capsule::schema()->create(Database::$prefix_self . '_order_price', function ($table) use ($foreignNameOrder, $foreignNamePrice, $foreignNameAppointments) {
-            $table->increments('id');
-            $table->unsignedInteger('order_id');
-            $table->unsignedInteger('price_id');
-            $table->string('item_name');
-            $table->unsignedMediumInteger('price_value');
-            $table->unsignedInteger('appointment_id')->nullable()->default(null);
-            $table->foreign('order_id', $foreignNameOrder)->references('id')->on(Database::$prefix_self . '_orders');
-            $table->foreign('price_id', $foreignNamePrice)->references('id')->on(Database::$prefix_self . '_prices');
-            $table->foreign('appointment_id', $foreignNameAppointments)->references('id')->on(Database::$prefix_self . '_appointments');
-            $table->softDeletes();
-            $table->timestamps();
-        });
+        if (!Capsule::schema()->hasTable(Database::$prefix_self . '_order_price')) {
+            Capsule::schema()->create(Database::$prefix_self . '_order_price', function ($table) use ($foreignNameOrder, $foreignNamePrice, $foreignNameAppointments) {
+                $table->increments('id');
+                $table->unsignedInteger('order_id');
+                $table->unsignedInteger('price_id');
+                $table->string('item_name');
+                $table->unsignedMediumInteger('price_value');
+                $table->unsignedInteger('appointment_id')->nullable()->default(null);
+                $table->foreign('order_id', $foreignNameOrder)->references('id')->on(Database::$prefix_self . '_orders');
+                $table->foreign('price_id', $foreignNamePrice)->references('id')->on(Database::$prefix_self . '_prices');
+                $table->foreign('appointment_id', $foreignNameAppointments)->references('id')->on(Database::$prefix_self . '_appointments');
+                $table->softDeletes();
+                $table->timestamps();
+            });
+        }
     }
 
 
     protected function getFKOrders()
     {
-        return $this->getForeignName(Database::$prefix_self . '_order_price_order_id_foreign');
+        return $this->getForeignName(Database::$prefix_self . '_orderp_oid_foreign');
     }
 
     protected function getFKPrices()
     {
-        return $this->getForeignName(Database::$prefix_self . '_order_price_price_id_foreign');
+        return $this->getForeignName(Database::$prefix_self . '_orderp_pid_foreign');
     }
 
     protected function getFKAppointments()
     {
-        return $this->getForeignName(Database::$prefix_self . '_order_price_appointment_id_foreign');
+        return $this->getForeignName(Database::$prefix_self . '_orderp_aid_foreign');
     }
 
     /**
