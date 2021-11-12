1f5b8a36cfa3d7db80b6f9b196f6ae5a923cd526
Benjamin Caubere <b.caubere@gmail.com>
2021-11-12 12:04:39 +0100

    proccess error message with links

--- app/Installation/Checks/Php.php
+++ app/Installation/Checks/Php.php
@@ -17,7 +17,9 @@ class Php extends \Wappointment\Installation\MethodsRunner
 
     protected function canRunPhp()
     {
-
+        throw new \WappointmentException(
+            'Wappointment is not yet compatible with PHP 8 yet. You can install our PHP 8 beta version following this guide: https://wappointment.com/docs/installing-php8-version/'
+        );
         if (version_compare(PHP_VERSION, WAPPOINTMENT_PHP_MIN) < 0) {
             throw new \WappointmentException(
                 'Your website\'s PHP version(' . PHP_VERSION . ') is lower to our minimum requirement '
@@ -27,7 +29,7 @@ class Php extends \Wappointment\Installation\MethodsRunner
         $max = '8.0.0';
         if (version_compare(PHP_VERSION, $max) >= 0) {
             throw new \WappointmentException(
-                'This version of Wappointment is not yet compatible with PHP 8 yet. You can install our PHP 8 beta version following this guide: https://wappointment.com/docs/installing-php8-version/'
+                'Wappointment is not yet compatible with PHP 8 yet. You can install our PHP 8 beta version following this guide: https://wappointment.com/docs/installing-php8-version/'
             );
         }
     }
---
+++ resources/js/Wappointment/ProcessMessage.vue
@@ -0,0 +1,28 @@
+<template>
+    <span>
+      {{ messageParsed }} <a v-if="linkFound" :href="linkFound" target="_blank">{{ linkFound }}</a>
+    </span>
+</template>
+
+<script>
+
+export default {
+    props: ['message'],
+    data: () => ({
+        linkFound: false,
+    }),
+    computed: {
+      messageParsed(){
+        let lookForLink = 'https://'
+        if(this.message.indexOf(lookForLink) === -1 ){
+          return this.message
+        }else{
+          let split = this.message.split(lookForLink)
+          this.linkFound = lookForLink + split[1]
+          return split[0]
+        }
+      }
+    },
+    
+}
+</script>
