ccbf220cf0aaa0240dfcf480ea384c9a7e2b1bbb
ben <ben@wappointment.com>
2022-03-09 18:43:45 +0100

    fixed bug availability non fluid today first load

--- resources/js/BookingForm/Calendar.vue
+++ resources/js/BookingForm/Calendar.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="updatedIntervals !== false">
+    <div v-if="updatedIntervals !== false && started">
         <CalendarWeek v-if="isWeekView" @selectSlot="selectSlot" :options="options" :service="service" :initIntervalsCollection="updatedIntervals"
                     :timeprops="timeprops" :staffs="staffs" :duration="duration" :location="location" :viewData="viewData"  />
         <CalendarMonth v-else @selectSlot="selectSlot" :options="options" :service="service" :initIntervalsCollection="updatedIntervals"
@@ -18,7 +18,8 @@ export default {
     props: ['options','service','initIntervalsCollection', 'timeprops', 'staffs', 'duration', 'location', 'viewData','rescheduling', 'relations'],
     data: () => ({
         updatedIntervals: false,
-        intervalId: false
+        intervalId: false,
+        started: false
     }),
     mixins: [Dates, DatesExtended],
     components: {
@@ -39,32 +40,62 @@ export default {
             //if today has slots we register an event
             if(this.isTSToday(this.updatedIntervals.intervals[0].start)){
                 this.refreshIntervals()
+                setTimeout(this.initTodayInterval,  100)
                 this.intervalId = setInterval(this.refreshIntervals, 60 * 1000)
+            }else{
+                this.started = true
             }
-            
-            
+        },
+        
+        /**
+         * avoid screen flashing on first load
+         */
+        initTodayInterval(){
+            this.refreshIntervals()
+            this.started = true
         },
         
         refreshIntervals(){
             // min is now 
             let newStart = this.getMinStart().unix()
             this.updatedIntervals = false
-            setTimeout(this.setNewValue.bind(null,newStart), 100);
+            setTimeout(this.setNewValue.bind(null,newStart), 50)
         },
         setNewValue(newStart){
             let newIntervals = this.initIntervalsCollection
-            if(newIntervals.intervals[0].start < newStart){
-                if(this.viewData.availability_fluid){
-                    newIntervals.intervals[0].start = newStart //we get automatically the first value with 5 min increments
-                }else{
-                     while (newIntervals.intervals[0].start < this.now.unix()) { //standard increment by duration selected
-                         newIntervals.intervals[0].start += this.duration *60
-                     }
-                }
-                
-                
-            }
+            newIntervals = this.incrementStartingValue(newIntervals, newStart) //increment 
+            newIntervals = this.cleanStartingFromEnd(newIntervals, newStart)
+            
             this.updatedIntervals = newIntervals
+        },
+
+        cleanStartingFromEnd(newIntervals, newStart){
+            let intervals = newIntervals.intervals
+            for (let i = 0; i < intervals.length; i++) {
+                const element = intervals[i]
+                if(element.end < newStart){
+                    intervals.splice(i, 1)
+                }
+            }
+            newIntervals.intervals = intervals
+            return newIntervals
+        },
+        /**
+         * increment starting value for today's date until it's an acceptable value
+         */
+        incrementStartingValue(newIntervals, newStart){
+            let intervals = newIntervals.intervals
+            if(intervals[0].start < newStart){
+                if(this.viewData.availability_fluid){
+                    intervals[0].start = newStart //we get automatically the first value with 5 min increments
+                }else{
+                     while (intervals[0].start < this.now.unix()) { //standard increment by duration selected
+                         intervals[0].start += this.duration *60
+                    }
+                }
+            }
+            newIntervals.intervals = intervals
+            return newIntervals
         },
         selectSlot(slot){
             let next = this.relations.next
