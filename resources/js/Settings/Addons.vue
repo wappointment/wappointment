32b467c884710698c7596b6be1d36d853110f00c
ben <ben@wappointment.com>
2022-11-11 13:20:00 +0100

    feat: added addon settings tab

--- resources/js/Settings/Addons.vue
+++ resources/js/Settings/Addons.vue
@@ -1,11 +1,13 @@
 <template>
   <div v-if="dataLoaded">
-    
     <div class="reduced">
+      <div v-for="addon in addonsWithSettings">
+        <button class="btn btn-secondary btn-sm" @click="show=addon['componentKey']">Edit settings - {{addon['name']}}</button>
+      </div>
       <WapModal :show="show" @hide="hidePopup" v-if="show">
           <h4 slot="title" class="modal-title">Edit addons settings</h4>
           <div class="mt-4">
-              <component is="WappointmentMailChimpSettings" />
+              <component :is="show" />
           </div>
       </WapModal>
     </div>
@@ -32,8 +34,20 @@ export default {
   data() {
     return {
       viewName: 'settingsaddons',
+      show:false,
+      addons: window.wappointmentExtends.filter('AddonsActive', window.wappointmentAdmin.addons )
     };
   },
+  computed:{
+    addonsWithSettings(){
+      return Object.values(this.addons).filter(e => e['hasAddonsSettings']===true)
+    }
+  },
+  methods:{
+    hidePopup(){
+      this.show = false
+    }
+  }
 
 };
 </script>
