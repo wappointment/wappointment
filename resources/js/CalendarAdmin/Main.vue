846a24be34f301f1dc3b0705d61ee7fc119f732b
ben <ben@wappointment.com>
2023-01-22 15:38:08 +0100

    fix: issue refresh when start of the week is not monday

--- resources/js/CalendarAdmin/Main.vue
+++ resources/js/CalendarAdmin/Main.vue
@@ -845,7 +845,7 @@ export default {
           this.queryParameters = {
               page: 'wappointment_calendar',
               start: this.firstDay.format(),
-              end: this.firstDay.clone().add(7, 'days').format(),
+              end: this.lastDay.format(),
               timezone: this.displayTimezone,
               staff: this.activeStaff.id,
             }
