02ddc66f239a3866ed8b409f908c4649d7255a57
ben <ben@wappointment.com>
2023-07-06 19:29:56 +0200

    fix: issue luxon reschedule

--- resources/js/BookingForm/RescheduleConfirm.vue
+++ resources/js/BookingForm/RescheduleConfirm.vue
@@ -3,7 +3,7 @@
         <div v-if="isCompactHeader" class="mb-2">
             <div class="wselected wclosable wmy-4 d-flex align-items-center d-flex-inline">
                 <WapImage :faIcon="['far','clock']" size="auto" />
-                <span class="welementname wml-2">{{ getMoment(selectedSlot.start, timeprops.currentTz).format(timeprops.fullDateFormat) }}</span>
+                <span class="welementname wml-2">{{ getLuxonFormated(selectedSlot.start) }}</span>
             </div>
         </div>
         <div class="d-flex wbtn-confirm my-2">
@@ -15,6 +15,7 @@
 
 <script>
 import Dates from "../Modules/Dates"
+import { DateTime } from "luxon";
 import AbstractFront from "./AbstractFront"
 export default {
     extends: AbstractFront,
@@ -29,7 +30,9 @@ export default {
         back(){
             this.$emit('back', this.relations.prev, {selectedSlot:false})
         },
-
+        getLuxonFormated(start){
+            return DateTime.fromSeconds(start).toLocaleString(DateTime.DATETIME_MED)
+        },
         confirm(){
             let data = {
                 appointmentkey: this.timeprops.appointmentkey,
