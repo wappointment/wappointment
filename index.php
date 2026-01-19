cf30fbc0c428228a207ab828b21d6b5fca2b5f0c
Ben C <b.caubere@gmail.com>
2026-01-19 18:53:24 +0100

    feat: v3 init : OK

--- index.php
+++ index.php
@@ -54,13 +54,25 @@ if (version_compare(PHP_VERSION, WAPPOINTMENT_PHP_MIN, '<')) {
     return;
 }
 
-// Load Composer autoloader
-require_once WAPPOINTMENT_PATH . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
+// Simple PSR-4 autoloader for Wappointment namespace
+spl_autoload_register(function ($class) {
+    $prefix = 'Wappointment\\';
+    $base_dir = WAPPOINTMENT_PATH . 'app/';
+    
+    $len = strlen($prefix);
+    if (strncmp($prefix, $class, $len) !== 0) {
+        return;
+    }
+    
+    $relative_class = substr($class, $len);
+    $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
+    
+    if (file_exists($file)) {
+        require $file;
+    }
+});
 
 // Initialize plugin
-add_action('plugins_loaded', function () {
-    new \Wappointment\System\Init();
-});
 add_action('plugins_loaded', function () {
     new \Wappointment\System\Init();
 });
